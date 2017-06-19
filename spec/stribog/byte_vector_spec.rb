4711b40bf65091ac7d6e680c6dacbf37e22a5757
Dmitrii Topornin <dtopornin@gmail.com>
2017-06-19 14:28:51 +0300

    refactoring rspec

--- spec/stribog/byte_vector_spec.rb
+++ spec/stribog/byte_vector_spec.rb
@@ -19,27 +19,27 @@ describe Stribog::ByteVector do
     let(:string1) { 'qwerty' }
     let(:string2) { '' }
 
-    it 'should return convert instance correct' do
+    it 'does return convert instance correct' do
       expect(subject.convert(string1).vector).to eq([113, 119, 101, 114, 116, 121])
       expect(subject.convert(string2).vector).to eq([])
     end
   end
 
   context '#+' do
-    it 'should return new vector contains concatenation of two' do
+    it 'does return new vector contains concatenation of two' do
       expect((vector1 + vector2).vector).to eq([113, 119])
       expect((vector1 + empty_vector).vector).to eq([113])
       expect((empty_vector  + empty_vector).vector).to eq([])
     end
 
-    it 'should return new object' do
+    it 'does return new object' do
       expect((vector1 + vector2).object_id).not_to eq(vector1.object_id)
       expect((vector1 + vector2).object_id).not_to eq(vector2.object_id)
     end
   end
 
   context '#^' do
-    it 'should return new vector contains xor of two' do
+    it 'does return new vector contains xor of two' do
       expect((vector1 ^ vector2).vector).to eq([6])
       expect((vector1 ^ empty_vector).vector).to eq([113])
       expect((vector3 ^ vector4).vector).to eq((vector4 ^ vector3).vector)
@@ -52,7 +52,7 @@ describe Stribog::ByteVector do
   context '#addition' do
     let(:vector) { subject.convert '1234567890' }
 
-    it 'should return new vector addition by zeros' do
+    it 'does return new vector addition by zeros' do
       expect((vector.addition).size).to eq(64)
       expect((vector.addition(size: 32)).size).to eq(32)
       expect((vector.addition(size: 16).vector)).to eq([0, 0, 0, 0, 0, 0, 49, 50, 51, 52, 53, 54, 55, 56, 57, 48])
@@ -62,7 +62,7 @@ describe Stribog::ByteVector do
   context '#padding' do
     let(:vector) { subject.convert '1234567890' }
 
-    it 'should return new vector addition by padding' do
+    it 'does return new vector addition by padding' do
       expect((vector.padding).size).to eq(64)
       expect((vector.padding(size: 32)).size).to eq(32)
       expect((vector.padding(size: 16).vector)).to eq([0, 0, 0, 0, 0, 1, 49, 50, 51, 52, 53, 54, 55, 56, 57, 48])
